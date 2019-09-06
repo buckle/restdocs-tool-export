fd5512add9c71b79e3a01d8ef0efd69cc80aec8d
Stacy Peace <stacy.peace@buckle.com>
2019-09-06 13:42:47 -0500

    Added Postman Url creator

---
+++ src/main/java/restdocs/tool/export/postman/exporter/Url.java
@@ -0,0 +1,62 @@
+package restdocs.tool.export.postman.exporter;
+
+import java.util.List;
+import java.util.Set;
+
+public class Url {
+
+  private String raw;
+  private String protocol;
+  private List<String> host;
+  private Integer port;
+  private List<String> path;
+  private Set<QueryParam> queryParams;
+
+  public String getRaw() {
+    return raw;
+  }
+
+  public void setRaw(String raw) {
+    this.raw = raw;
+  }
+
+  public String getProtocol() {
+    return protocol;
+  }
+
+  public void setProtocol(String protocol) {
+    this.protocol = protocol;
+  }
+
+  public List<String> getHost() {
+    return host;
+  }
+
+  public void setHost(List<String> host) {
+    this.host = host;
+  }
+
+  public Integer getPort() {
+    return port;
+  }
+
+  public void setPort(Integer port) {
+    this.port = port;
+  }
+
+  public List<String> getPath() {
+    return path;
+  }
+
+  public void setPath(List<String> path) {
+    this.path = path;
+  }
+
+  public Set<QueryParam> getQueryParams() {
+    return queryParams;
+  }
+
+  public void setQueryParams(Set<QueryParam> queryParams) {
+    this.queryParams = queryParams;
+  }
+}
